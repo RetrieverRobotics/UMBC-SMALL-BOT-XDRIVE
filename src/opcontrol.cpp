22c34d805d62770e259af9fbc11dce9a0b44d7eb
Lily-bugs <lillianfuller788@gmail.com>
2026-02-05 01:16:54 -0500

    updated code from update-code branch

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -7,187 +7,260 @@
  */
 
 #include "api.h"
+#include "pros/llemu.hpp"
+#include "pros/misc.h"
+#include "pros/motors.h"
+#include "pros/rtos.hpp"
+#include "pros/adi.hpp"
 #include "umbc.h"
-
-#include <cstdint>
+#include "umbc/robot.hpp"
 #include "okapi\api\control\iterative\iterativePosPidController.hpp"
+#include "okapi\impl\util\configurableTimeUtilFactory.hpp"
+#include <cstdint>
+#include <time.h>
+#include <vector>
+#include <cmath>
 
 using namespace pros;
 using namespace umbc;
 using namespace std;
-using namespace okapi;
 
+  
+  
+#define MOTOR_RED_GEAR_MULTIPLIER       100
+#define MOTOR_GREEN_GEAR_MULTIPLIER     200
 #define MOTOR_BLUE_GEAR_MULTIPLIER      600
-#define MOTOR_RED_GEAR_MULTIPLIER       300
+#define MOTOR_REVERSE                   true
+#define REVERSED(port)                  -port
 
-#define MOTOR_FRONT_L       14
-#define MOTOR_FRONT_R       -13
-#define MOTOR_BACK_L        12
-#define MOTOR_BACK_R        -11
+#define INTAKE_MOTOR_SPEED              100
+#define TARGET_ERROR                    10
+ 
+ 
+//left and right are relative to the robot's left and right
+ 
+//DOUBLE CHECK MOTORS!!!!!
+  
+//ports for left motors(blue)
+#define LEFT_MOTOR_FRONT        2
+#define LEFT_MOTOR_BACK         11
+       
+  
+//ports for right motors(blue)
+#define RIGHT_MOTOR_FRONT      -6
+#define RIGHT_MOTOR_BACK       -20
 
-#define ARM_MOTOR_R         -15
-#define ARM_MOTOR_L         17
+//ports for lift motors
+#define ARM_MOTOR_RIGHT        -21
+#define ARM_MOTOR_LEFT          5
 
-#define REST_POSITION       0
-#define GOAL_1_POSITION     -100
-#define GOAL_2_POSITION     -300
-#define GOAL_3_POSIITON     -800
+//ports for intake motors
+#define INTAKE_MOTOR_LEFT       12
+#define INTAKE_MOTOR_RIGHT     -18
 
-#define KP                  3
-#define KD                  0
-#define KI                  0
-#define KBIAS               0
+#define REST_POSITION        -100       //low goal
+#define MID_GOAL_POSITION   -850    //mid goal
 
 
-ARM_STATE cur_state = ARM_STATE::REST;
-DIGITAL_BUTTON_STATE prev_button_state_r1 = DIGITAL_BUTTON_STATE::BUTTON_RELEASED;
-DIGITAL_BUTTON_STATE cur_button_state_r1 = DIGITAL_BUTTON_STATE::BUTTON_RELEASED;
+#define KP                   3
+#define KD                   0
+#define KI                   0
+#define KBIAS                0
 
-DIGITAL_BUTTON_STATE prev_button_state_r2 = DIGITAL_BUTTON_STATE::BUTTON_RELEASED;
-DIGITAL_BUTTON_STATE cur_button_state_r2 = DIGITAL_BUTTON_STATE::BUTTON_RELEASED;
 
 void umbc::Robot::opcontrol() {
-    
+
     // nice names for controllers (do not edit)
     umbc::Controller* controller_master = this->controller_master;
     umbc::Controller* controller_partner = this->controller_partner;
 
-    IterativePosPIDController arm_controler = IterativePosPIDController({KP, KI, KD, KBIAS}, global_time);
+    //initialize PID controller for arm
+    okapi::ConfigurableTimeUtilFactory global_time_factory;
+    okapi::TimeUtil global_time = global_time_factory.create();
+    okapi::IterativePosPIDController arm_controller(KP, KI, KD, KBIAS, global_time);
     
-    // initialize motors and sensors
-    const vector<int8_t> motors = {ARM_MOTOR_L, ARM_MOTOR_R};
-    pros::Motor_Group arm_group = Motor_Group(motors);
+    //initialize motors
+    std::vector<int8_t> drive_motors{LEFT_MOTOR_FRONT, LEFT_MOTOR_BACK, RIGHT_MOTOR_FRONT, RIGHT_MOTOR_BACK};
+    std::vector<int8_t> intake_motors{INTAKE_MOTOR_LEFT, INTAKE_MOTOR_RIGHT};
+    std::vector<int8_t> arm_motors{ARM_MOTOR_LEFT, ARM_MOTOR_RIGHT};
+    pros::MotorGroup driveGroup (drive_motors);
+    pros::MotorGroup intakeGroup (intake_motors);
+    pros::MotorGroup armGroup (arm_motors);
 
-    //arm_group.set_brake_modes(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_BRAKE);
+    //brakes and gearing
+    //DRIVE
+    driveGroup.set_brake_modes(E_MOTOR_BRAKE_BRAKE);
+    driveGroup.set_gearing(E_MOTOR_GEAR_GREEN);
 
-    pros::Motor m1 = Motor(MOTOR_FRONT_L);
-    pros::Motor m2 = Motor(MOTOR_FRONT_R);
-    pros::Motor m3 = Motor(MOTOR_BACK_L);
-    pros::Motor m4 = Motor(MOTOR_BACK_R);
+    //INTAKE
+    intakeGroup.set_brake_modes(E_MOTOR_BRAKE_COAST);
+    intakeGroup.set_gearing(E_MOTOR_GEAR_BLUE);
 
-    pros::Motor m5 = Motor(ARM_MOTOR_R);
-    pros::Motor m6 = Motor(ARM_MOTOR_L);
+    //ARM
+    armGroup.set_brake_modes(E_MOTOR_BRAKE_BRAKE);
+    armGroup.set_gearing(E_MOTOR_GEAR_RED);
 
-    //set current and prev arm state
-    cur_button_state_r1 = prev_button_state_r1;
-    cur_button_state_r2 = prev_button_state_r2;
-    int state_selector = 0; //inc. or dec. variable the user changes to operate the arm state machine 
     
+    //drive motors
+    pros::Motor left_motor_front (LEFT_MOTOR_FRONT);
+    pros::Motor left_motor_back (LEFT_MOTOR_BACK);
+        
+    pros::Motor right_motor_front (RIGHT_MOTOR_FRONT);
+    pros::Motor right_motor_back (RIGHT_MOTOR_BACK);
+
+    //intake motors
+    pros::Motor intake_motor_left (INTAKE_MOTOR_LEFT);
+    pros::Motor intake_motor_right (INTAKE_MOTOR_RIGHT);
+
+    //motor states
+    bool slowSpeedButton = false;
+
+    enum class INTAKE_STATE {INTAKE_OFF, INTAKE_ON, INTAKE_REVERSE};
+    INTAKE_STATE intakeState = INTAKE_STATE::INTAKE_OFF;
+    
+    enum class ARM_STATE {REST, MID_GOAL};  //implement HIGH_GOAL if the bot can reach it
+    ARM_STATE cur_state = ARM_STATE::REST;
+    int state_selector = 0;
 
     while(1) {
+        //left joystick (target movement)
+        double left_x = controller_master->get_analog(E_CONTROLLER_ANALOG_LEFT_X);
+        double left_y = controller_master->get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+        //controller deadzone
+        if(left_x > -15 && left_x < 15){
+            left_x = 0;
+        }
+        if(left_y > -15 && left_y < 15){
+            left_y = 0;
+        }
+        left_x = pow(left_x, 3) / (127 * 127 * 127); //cubing for finer control
+        left_y = pow(left_y, 3) / (127 * 127 * 127); //cubing for finer control
 
-        float analog_left_x = controller_master->get_analog(E_CONTROLLER_ANALOG_LEFT_X);
-        float analog_left_y = controller_master->get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
-        float analog_right_x = controller_master->get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
-
+        //right joystick (rotation)
+        double right_x = controller_master->get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+        right_x = pow(right_x, 3) / (127 * 127 * 127) *0.75; //cubing for finer control
         
-        float S = 0;
-        S = sqrt(pow(analog_left_x,2) + pow(analog_left_y,2)); //I think this would calculate distance...idk
+        //converting to polar
+        double radius = sqrt(left_x * left_x + left_y * left_y);
+        double theta  = atan2(left_y, left_x);
 
-        float T = 0;
-        if(!(S == 0)){
-            T = atan2(analog_left_y, analog_left_x);
-        }
-
-        float R = 0;
-        R = analog_right_x;
-
+        //this will determine the speed of each motor
+        double power_front_left  = sin(theta + (M_PI)/4) / cos(M_PI/4);
+        double power_front_right = -cos(theta + (M_PI/4)) / cos(M_PI/4);
+        double power_back_left   = -cos(theta + (M_PI/4)) / cos(M_PI/4);
+        double power_back_right  = sin(theta + (M_PI)/4) / cos(M_PI/4); 
         
-        float P_1 = 0;
-        P_1 = -(cos(T + (M_PI/4))/cos(M_PI/4));
+        double speed = 0;
+        if(radius != 0){
+            speed = max({fabs(power_back_left), fabs(power_back_right), fabs(power_front_left), fabs(power_front_right)}) / radius;
+            //speed2 = max(fabs(power_front_left), fabs(power_front_right));
+            //speed = max(speed1, speed2) / radius;
+        }
 
-        float P_2 = 0;
-        P_2 = -(cos(T + (3*M_PI/4))/cos(M_PI/4));
-
-        float P_lf = 0;
-        float P_rf = 0;
-        float P_lb = 0;
-        float P_rb = 0;
-        P_lf = P_2;
-        P_rf = P_1;
-        P_lb = P_1;
-        P_rb = P_2;
-
-
-        float M_lf = 0;
-        float M_rf = 0;
-        float M_lb = 0;
-        float M_rb = 0;
-        float largest_stick = 0;
+        double vel_fl = 0;
+        double vel_fr = 0;
+        double vel_bl = 0;
+        double vel_br = 0;
+        if(speed != 0){
+            vel_fl = power_front_left;
+            vel_fr = power_front_right;
+            vel_bl = power_back_left;
+            vel_br = power_back_right;
+        }
         
-        if(S != 0){
-            largest_stick = abs((std::max(std::max(P_lf, P_rf), std::max(P_lb, P_rb))))/S;
+        //SPEED CONTROL
+        if(controller_master->get_digital_new_press(E_CONTROLLER_DIGITAL_UP)){ //toggle slow speed
+            slowSpeedButton = false;
         }
-
-        if((largest_stick != 0)){
-            M_lf = (P_lf/(largest_stick))*(1-abs(R))+R;
-            M_rf = (P_rf/(largest_stick))*(1-abs(R))-R;
-            M_lb = (P_lb/(largest_stick))*(1-abs(R))+R;
-            M_rb = (P_rb/(largest_stick))*(1-abs(R))-R;
+        if(controller_master->get_digital_new_press(E_CONTROLLER_DIGITAL_DOWN)){ //toggle slow speed
+            slowSpeedButton = true;
         }
-
-        if(controller_master->get_digital(E_CONTROLLER_DIGITAL_R1)) //move arm up
-        {
-            cur_button_state_r1 = DIGITAL_BUTTON_STATE::BUTTON_PRESSED;
-        }else{
-            cur_button_state_r1 = DIGITAL_BUTTON_STATE::BUTTON_RELEASED;
+        
+        //INTAKE CONTROLS
+        if(controller_master->get_digital_new_press(E_CONTROLLER_DIGITAL_R2)){ //toggle intake on
+            if(intakeState == INTAKE_STATE::INTAKE_ON){
+                intakeState = INTAKE_STATE::INTAKE_OFF;
+            }
+            else{
+                intakeState = INTAKE_STATE::INTAKE_ON;
+            }
         }
-        if(controller_master->get_digital(E_CONTROLLER_DIGITAL_R2)) //move arm down
-        {
-            cur_button_state_r2 = DIGITAL_BUTTON_STATE::BUTTON_PRESSED;
-        }else{
-            cur_button_state_r2 = DIGITAL_BUTTON_STATE::BUTTON_RELEASED;
+        if(controller_master->get_digital_new_press(E_CONTROLLER_DIGITAL_R1)){ //toggle intake reverse
+            if(intakeState == INTAKE_STATE::INTAKE_REVERSE){
+                intakeState = INTAKE_STATE::INTAKE_OFF;
+            }
+            else{
+                intakeState = INTAKE_STATE::INTAKE_REVERSE;
+            }
         }
-
-        if((cur_button_state_r1 == DIGITAL_BUTTON_STATE::BUTTON_PRESSED) && (prev_button_state_r1 == DIGITAL_BUTTON_STATE::BUTTON_RELEASED)){
+    
+        //LIFT CONTROLS
+        if(controller_master->get_digital_new_press(E_CONTROLLER_DIGITAL_L1)){ //move arm up
             state_selector++;
         }
-        if((cur_button_state_r2 == DIGITAL_BUTTON_STATE::BUTTON_PRESSED) && (prev_button_state_r2 == DIGITAL_BUTTON_STATE::BUTTON_RELEASED)){
+
+        if(controller_master->get_digital_new_press(E_CONTROLLER_DIGITAL_L2)){ //move arm down
             state_selector--;
         }
 
-        if(state_selector > 3){
-            state_selector = 3;
+        if(state_selector > 1){
+            state_selector = 1;
         }else if (state_selector < 0){
             state_selector = 0;
         }
         cur_state = (ARM_STATE)state_selector;
 
         switch (cur_state){
-            case REST:
-                arm_controler.setTarget(REST_POSITION);
+            case ARM_STATE::REST:
+                arm_controller.setTarget(REST_POSITION);
                 break;
             
-            case GOAL_1:
-                arm_controler.setTarget(GOAL_1_POSITION);
-                break;
-            
-            case GOAL_2:
-                arm_controler.setTarget(GOAL_2_POSITION);
-                break;
-            
-            case GOAL_3:
-                arm_controler.setTarget(GOAL_3_POSIITON);
+            case ARM_STATE::MID_GOAL:
+                arm_controller.setTarget(MID_GOAL_POSITION);
                 break;
         }
-                    
-        arm_group.move_absolute(arm_controler.getTarget(), -50 * arm_controler.getOutput());
-
-        m1.move_velocity((M_lf+R) * MOTOR_BLUE_GEAR_MULTIPLIER * .01);
-        m2.move_velocity((M_rf-R) * MOTOR_BLUE_GEAR_MULTIPLIER * .01);
-        m3.move_velocity((M_lb+R) * MOTOR_BLUE_GEAR_MULTIPLIER * .01);
-        m4.move_velocity((M_rb-R) * MOTOR_BLUE_GEAR_MULTIPLIER * .01);
         
-        pros::lcd::set_text(1, (std::to_string(m5.get_position()))); //right motor
-        pros::lcd::set_text(2, (std::to_string(m6.get_position()))); //left motor
 
-        arm_controler.step((arm_group.get_positions()[0] + arm_group.get_positions()[1])/2);
+        //MOVING MOTORS
+        //slow speed if toggled on
+        if(slowSpeedButton){
+            vel_fl *= 0.5;
+            vel_fr *= 0.5;
+            vel_bl *= 0.5;
+            vel_br *= 0.5;
+        }
         
-        prev_button_state_r1 = cur_button_state_r1;
-        prev_button_state_r2 = cur_button_state_r2;
+        //intake motors
+        switch(intakeState){
+            case INTAKE_STATE::INTAKE_OFF:
+                intake_motor_left.move_velocity(0);
+                intake_motor_right.move_velocity(0);
+                break;
+            case INTAKE_STATE::INTAKE_ON:
+                intake_motor_left.move_velocity(MOTOR_BLUE_GEAR_MULTIPLIER * 0.85);
+                intake_motor_right.move_velocity(MOTOR_BLUE_GEAR_MULTIPLIER * 0.85);
+                break;
+            case INTAKE_STATE::INTAKE_REVERSE:
+                intake_motor_left.move_velocity(-MOTOR_BLUE_GEAR_MULTIPLIER * 0.85);
+                intake_motor_right.move_velocity(-MOTOR_BLUE_GEAR_MULTIPLIER * 0.85);
+                break;
+        }
+        
+        //drive motors
+        left_motor_front.move_velocity((vel_fl + right_x)*MOTOR_GREEN_GEAR_MULTIPLIER*0.7);
+        left_motor_back.move_velocity((vel_bl + right_x)*MOTOR_GREEN_GEAR_MULTIPLIER*0.7);
+        right_motor_front.move_velocity((vel_fr - right_x)*MOTOR_GREEN_GEAR_MULTIPLIER*0.7);
+        right_motor_back.move_velocity((vel_br - right_x)*MOTOR_GREEN_GEAR_MULTIPLIER*0.7);
 
-        pros::lcd::set_text(3, (std::to_string(arm_controler.getTarget())));
-        pros::lcd::set_text(4, (std::to_string(arm_controler.getOutput())));
+        //lift motors
+        //error system to prevent arm motors from moving while within a close enough range of the target (UNTESTED)
+        if(abs((arm_controller.getTarget() - ((armGroup.get_positions()[0] + armGroup.get_positions()[1])/2))) > TARGET_ERROR){
+            armGroup.move_absolute(arm_controller.getTarget(), -MOTOR_RED_GEAR_MULTIPLIER * arm_controller.getOutput()*0.25);  //DO NOT CHANGE THIS VALUE WITHOUT TESTING
+        }else{
+            armGroup.move_velocity(0);
+        }
+        arm_controller.step((armGroup.get_positions()[0] + armGroup.get_positions()[1])/2);
+        
 
         // required loop delay (do not edit)
         pros::Task::delay(this->opcontrol_delay_ms);
