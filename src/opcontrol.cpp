f1e86bf852b2f48fc455720084f13a0e5c8a248e
Lily-bugs <lillianfuller788@gmail.com>
2026-02-20 21:16:20 -0500

    changed port 15 to 14

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -102,7 +102,7 @@ class PIDController {
 
 //ports for intake motors (blue)
 #define INTAKE_MOTOR_LEFT                   12
-#define INTAKE_MOTOR_RIGHT                  -15
+#define INTAKE_MOTOR_RIGHT                  -14
 
 #define INTAKE_SINGLE_OUT_TIMMER       320
 
